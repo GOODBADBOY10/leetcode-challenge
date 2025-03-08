733df4e89e312bfe5a84e2c4d116a2ace66d4c02
GOODBADBOY10 <selfmanademola@gmail.com>
2025-03-08 23:35:02 +0100

    let's jump again

---
+++ Animations/src/App.jsx
@@ -0,0 +1,34 @@
+import './App.css'
+import { Canvas } from '@react-three/fiber'
+
+function App() {
+
+  return (
+    <>
+      <div id="canvas-container">
+        <Canvas>
+          <mesh>
+            <boxGeometry args={[4, 4, 4]} />
+            <meshPhongMaterial />
+          </mesh>
+          <ambientLight intensity={0.1} />
+          <directionalLight position={[0, 0, 5]} color="green" />
+        </Canvas>
+      </div>
+    </>
+  )
+}
+
+export default App
+
+
+
+
+
+
+// You can live-edit the code above:
+
+// try different materials, like MeshNormalMaterial or MeshBasicMaterial, give them a color
+// try different geometries, like SphereGeometry or OctahedronGeometry
+// try changing the position on our mesh component, by setting the prop with the same name
+// try extracting our mesh to a new component
--- jump.js
+++ jump.js
@@ -12,3 +12,31 @@ function jumpTime(nums) {
 // const nums = [2, 3, 1, 1, 4]
 const nums = [2, 3, 11]
 console.log(jumpTime(nums));
+
+
+
+
+
+
+
+// var canJump = function(nums) {
+//     let idx = 0;
+//     let max = 0;
+//     let target = nums.length - 1;
+  
+//     while(idx < nums.length) {
+//       max = Math.max(max, idx + nums[idx]);
+      
+//       if (max >= target) {
+//         return true;
+//       }
+      
+//       if (max <= idx && nums[idx] === 0) {
+//         return false;
+//       }
+      
+//       idx++;
+//     }
+    
+//     return false;
+//   };
